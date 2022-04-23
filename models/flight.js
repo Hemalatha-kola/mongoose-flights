bc1bb11f8c701755acf4f0dc6f226d51bb57983a
Hemalatha-kola <kola123hema@gmail.com>
2022-04-22 23:03:30 -0700

    initial commit

---
+++ models/flight.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema(
+    {
+    airline : {type: String, enum: ['American','Southwest','United']},
+    airport: {type: String, enum:['AUS','DFW','DEN','LAX','SAN'],default:'DEN'},
+    flightNo: Number, 
+    departs: Date
+});
+
+module.exports = mongoose.model('Flight',flightSchema);
